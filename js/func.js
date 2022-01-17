e2572b23ba96460b26d36e38194492920053e285
marcello <marcellopettenuzzo2@gmail.com>
2022-01-17 18:23:37 +0100

    elimina lista

--- js/func.js
+++ js/func.js
@@ -2,13 +2,31 @@ let app = new Vue ({
     el: '#vueApp',
     data:
     {
-        add: 'valore',
-        lista : 
+        aggiungi: '',
+        lista :
         [
             {
-                elemento : 'lista',
-                done : true
+                testo: 'nome',
+                done: false
             }
+
         ]
+
+    },
+    methods:
+    {
+        aggiungiLista : function()
+        {
+            this.lista.push({testo : this.aggiungi , done:false});
+        },
+        cambiaStato : function(index)
+        {
+            this.lista[index].done = !this.lista[index].done
+        },
+        rimuoviLista : function(index)
+        {
+            this.lista.splice(index , 1)
+        }
     }
+    
 });
